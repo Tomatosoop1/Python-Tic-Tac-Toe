7fa207a82cadfff181b0883ba712b11b2392087a
Luisssssssssss <100135822+Tomatosoop1@users.noreply.github.com>
2023-03-09 14:42:43 +0100

    Added check for winner
    Still some errors in winner checker

--- tictactoe.py
+++ tictactoe.py
@@ -33,6 +33,50 @@ def player_o():
             board[option_o] = "O"
             printBoard()
             break
+        
+def winner():
+    global board
+    #The first 3 check if player X has won on a row
+    if board[1] and board[2] and board[3] == "X":
+        print("Player X won!")
+    elif board[4] and board[5] and board[6] == "X":
+        print("Player X won!")
+    elif board[7] and board[8] and board[9] == "X":
+        print("Player X won!")
+    #The 3 after this check if player X has won in a column
+    elif board[1] and board[4] and board[7] == "X":
+        print("Player X won!")
+    elif board[2] and board[5] and board[8] == "X":
+        print("Player X won!")
+    elif board[3] and board[6] and board[9] == "X":
+        print("Player X won!")
+    #The next 2 check if player X won on the diagonal
+    elif board[1] and board[5] and board[9] == "X":
+        print("Player X has won!")
+    elif board[3] and board[5] and board[7] == "X":
+        print("Player X has won!")
+        #The first 3 check if player O has won on a row
+    elif board[1] and board[2] and board[3] == "O":
+        print("Player O won!")
+    elif board[4] and board[5] and board[6] == "O":
+        print("Player O won!")
+    elif board[7] and board[8] and board[9] == "O":
+        print("Player O won!")
+    #The 3 after this check if player O has won in a column
+    elif board[1] and board[4] and board[7] == "O":
+        print("Player O won!")
+    elif board[2] and board[5] and board[8] == "O":
+        print("Player O won!")
+    elif board[3] and board[6] and board[9] == "O":
+        print("Player O won!")
+    #The next 2 check if player O won on the diagonal
+    elif board[1] and board[5] and board[9] == "O":
+        print("Player O has won!")
+    elif board[3] and board[5] and board[7] == "O":
+        print("Player O has won!")
+    else:
+        print("It was a draw!")
+    
 
 printBoard()
 player_x()
@@ -44,7 +88,7 @@ player_o()
 player_x()
 player_o()
 player_x()
-
+winner()
 
 
 
