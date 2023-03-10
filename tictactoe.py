3001f4fc15ee5ea732ba2f91de05e12ead2ae510
Luisssssssssss <100135822+Tomatosoop1@users.noreply.github.com>
2023-03-10 12:42:20 +0100

    v0.4 Release
    NOTE: In this version you can not play the game but the code has seen very many improvements and will probably run in the next version

--- tictactoe.py
+++ tictactoe.py
@@ -1,55 +1,44 @@
 import time
+
+#List used later in the code for checking if moves are availabel
+AvailableOptions = [1,2,3,4,5,6,7,8,9]
+
+#This is a dictionary with currently nothing in it, later this is where the players moves are stored
 board = {
     1: " ", 2: " ", 3: " ",
     4: " ", 5: " ", 6: " ",
     7: " ", 8: " ", 9: " "
     }
+
+#These list all define different ways the players could win (if all variabels in any of them are the same that player wins)
+first_row = [board[1],board[2],board[3]]
+second_row = [board[4],board[5],board[6]]
+third_row = [board[7],board[8],board[9]]
+
+first_collumn = [board[1],board[4],board[7]]
+second = [board[2],board[5],board[8]]
+third_collumn = [board[3],board[6],board[9]]
+
+first_diagonal = [board[1],board[5],board[9]]
+second_diagonal = [board[3],board[5],board[7]]
+
+#WIP win checker
+def checkRow(first_row):
+    element = "X"
+    check = True
+
+    for item in first_row:
+        if element != item:
+            check = False
+            break
+
+#The typewrite functions doesnt serve a real purpose its purely cosmetic
 def typewrite(str):
     for x in str:
-        time.sleep(0.05,)
+        time.sleep(0.01)
         print(x,end="")
 
-def winner():
-    global board
-    #The first 3 check if player X has won on a row
-    if board[1] and board[2] and board[3] == "X":
-        print("Player X won!")
-    elif board[4] and board[5] and board[6] == "X":
-        print("Player X won!")
-    elif board[7] and board[8] and board[9] == "X":
-        print("Player X won!")
-    #The 3 after this check if player X has won in a column
-    elif board[1] and board[4] and board[7] == "X":
-        print("Player X won!")
-    elif board[2] and board[5] and board[8] == "X":
-        print("Player X won!")
-    elif board[3] and board[6] and board[9] == "X":
-        print("Player X won!")
-    #The next 2 check if player X won on the diagonal
-    elif board[1] and board[5] and board[9] == "X":
-        print("Player X has won!")
-    elif board[3] and board[5] and board[7] == "X":
-        print("Player X has won!")
-        #The first 3 check if player O has won on a row
-    elif board[1] and board[2] and board[3] == "O":
-        print("Player O won!")
-    elif board[4] and board[5] and board[6] == "O":
-        print("Player O won!")
-    elif board[7] and board[8] and board[9] == "O":
-        print("Player O won!")
-    #The 3 after this check if player O has won in a column
-    elif board[1] and board[4] and board[7] == "O":
-        print("Player O won!")
-    elif board[2] and board[5] and board[8] == "O":
-        print("Player O won!")
-    elif board[3] and board[6] and board[9] == "O":
-        print("Player O won!")
-    #The next 2 check if player O won on the diagonal
-    elif board[1] and board[5] and board[9] == "O":
-        print("Player O has won!")
-    elif board[3] and board[5] and board[7] == "O":
-        print("Player O has won!")
-
+#This just prints the current board layout
 def printBoard():
     global board
     print(" ",board[1] + "|" + " " + board[2] + " " + "|" + board[3])
@@ -59,71 +48,37 @@ def printBoard():
     print(" ",board[7] + "|" + " " + board[8] + " " + "|" + board[9])
 
 
-def player_x():
-    global board
-    while True:
-        option_x = int(input("What field would you like to choose?"))
-        if board[option_x] == "O":
-            print("NOT VALID")
-        else:
-            board[option_x] = "X"
-            printBoard()
-            winner()
-            break
+def playerX():
+    global AvailableOptions
+    print(AvailableOptions)
+    option = int(input("What field would you like to play in?\n"))
+    if option in AvailableOptions:
+        AvailableOptions.remove(option)
+        board[option] = "X"
+        printBoard()
+    else:
+        print("NOT AVAILABLE")
 
-def player_o():
-    global board
-    while True:
-        option_o = int(input("What field would you like to choose?"))
-        if board[option_o] == "X":
-            print("NOT VALID")
-        else:
-            board[option_o] = "O"
-            printBoard()
-            winner()
-            break
 
+def playerO():
+    global AvailableOptions
+    print(AvailableOptions)
+    option = int(input("What field would you like to play in?\n"))
+    if option in AvailableOptions:
+        AvailableOptions.remove(option)
+        board[option] = "O"
+        printBoard()
+    else:
+        print("NOT AVAILABLE")
+  
+#Just a small startup welcome menu/info screen
 def startup():
     typewrite("Welcome to Tic tac toe in Python - By Luis Wettre\n\n")
-    typewrite("""The Rules are simple, there are 2 players that take turns on choosing a field.\n
+    typewrite("""The Rules are simple, there are 2 players that take turns on choosing a field.
 The first player to reach 3 off their charachter in a row (Horizontal, Diagonal or Vertical) wins the game
-if no one manages to do this, the game is a draw!\n\n
+if no one manages to do this, the game is a draw!\n
 You pick the square you want to play in py choosing a number between 1 - 9, the first row being 1 - 3, second row 4 - 6 and third row 7 - 9\n\n""")
     
-def rounds():
-    player_x()
-    winner()
-    player_o()
-    player_x()
-    winner()
-    player_o()
-    player_x()
-    winner()
-    player_o()
-    player_x()
-    winner()
-    player_o()
-    winner()
-    player_x()
-
-
+#Here i call all functions to start the game
 startup()
 printBoard()
-rounds()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
